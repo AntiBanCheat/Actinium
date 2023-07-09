2e04f4a3988651af47dbcccc2fbe9ce3a39fc798
AntiBanCheat <94786995+AntiBanCheat@users.noreply.github.com>
2023-07-09 12:09:47 +0900

    Update TPAura.cpp

--- Client/PacketClient/Module/Modules/Combat/TPAura.cpp
+++ Client/PacketClient/Module/Modules/Combat/TPAura.cpp
@@ -3,11 +3,16 @@
 
 using namespace std;
 TPAura::TPAura() : IModule(0, Category::COMBAT, "Attacks entities from far ranges") {
-	registerEnumSetting("Mode", &mode, 0);
-	mode.addEntry("Multi", 0);
-	mode.addEntry("Switch", 1);
-	registerIntSetting("Delay", &delay, delay, 1, 50);
-	registerFloatSetting("Range", &range, range, 5.f, 250.f);
+	registerIntSetting("Range", &range, range, 5, 250);
+	registerIntSetting("Delay", &delay, delay, 0, 40);
+	registerIntSetting("BackDelay", &bdelay, bdelay, 0, 40);
+	registerIntSetting("Height", &height, height, 0, 5);
+	registerIntSetting("MultiAttack", &multi, multi, 1, 20);
+	registerBoolSetting("Once", &once, once);
+	registerBoolSetting("Behind", &behind, behind);
+	registerBoolSetting("voidCheck", &voidCheck, voidCheck);
+	registerBoolSetting("Spoof", &spoof, spoof);
+	registerBoolSetting("Back", &backsettings, backsettings);
 }
 
 const char* TPAura::getModuleName() {
@@ -47,8 +52,9 @@ void findEntTPA(C_Entity* currentEntity, bool isRegularEntity) {
 }
 
 void TPAura::onEnable() {
-	teleported = false; ticks = 0;
 	targetList.clear();
+	back = false;
+	check = 0;
 }
 
 struct CompareTargetEnArray {
@@ -61,266 +67,81 @@ struct CompareTargetEnArray {
 void TPAura::onTick(C_GameMode* gm) {
 	auto player = g_Data.getLocalPlayer();
 	if (player == nullptr) return;
-	ticks++;
-
-	int APS = 20;
+	if (!once) {
+		tick++;
+		if (tick < realdelay)
+			return;
+		tick = 0;
+	}
 	targetList.clear();
 	g_Data.forEachEntity(findEntTPA);
-
-	vec3_t entPos = *targetList[0]->getPos();
-	vec3_t pos = *player->getPos();
-	vec3_t movepos = *player->getPos();
-	int x = movepos.x;
-	int y = movepos.y;
-	int z = movepos.z;
-	int ex = entPos.x;
-	int ey = entPos.y;
-	int ez = entPos.z;
-	int gx = pos.x;
-	int gy = pos.y;
-	int gz = pos.z;
-	bool isplus = false;
-	int counter = 0;
-	//TP
-	if (!teleported) {
-		if (ticks < delay && ticks > 1) {
-			//X
-			counter = ex - x;
-			if (0 < ex - x) {
-				isplus = true;
-			}
-			else
-			{
-				isplus = false;
-				counter = 0 - counter;
-			}
-			for (int i = 0; i < counter; i++) {
-				if (isplus) {
-					x += 1;
-				}
-				else {
-					x -= 1;
-				}
-				C_MovePlayerPacket packet(g_Data.getLocalPlayer(), *g_Data.getLocalPlayer()->getPos()); packet.Position.x = x;
-				g_Data.getClientInstance()->loopbackPacketSender->sendToServer(&packet);
-				PlayerAuthInputPacket p;
-				p.pos.x = x;
-				//g_Data.getClientInstance()->loopbackPacketSender->sendToServer(&p);
-			}
-
-			//Y
-			counter = ey - y;
-			if (0 < ey - y) {
-				isplus = true;
-			}
-			else
-			{
-				isplus = false;
-				counter = 0 - counter;
-			}
-			for (int i = 0; i < counter; i++) {
-				if (isplus) {
-					y += 1;
-				}
-				else {
-					y -= 1;
-				}
-				C_MovePlayerPacket packet(g_Data.getLocalPlayer(), *g_Data.getLocalPlayer()->getPos()); packet.Position.y = entPos.y + 2;
-				g_Data.getClientInstance()->loopbackPacketSender->sendToServer(&packet);
-				PlayerAuthInputPacket p;
-				p.pos.y = y;
-				//g_Data.getClientInstance()->loopbackPacketSender->sendToServer(&p);
-			}
-
-			//Z
-			counter = ez - z;
-			if (0 < ez - z) {
-				isplus = true;
-			}
-			else
-			{
-				isplus = false;
-				counter = 0 - counter;
-			}
-			for (int i = 0; i < counter; i++) {
-				if (isplus) {
-					z += 1;
-				}
-				else {
-					z -= 1;
-				}
-				C_MovePlayerPacket packet(g_Data.getLocalPlayer(), *g_Data.getLocalPlayer()->getPos()); packet.Position.z = z;
-				g_Data.getClientInstance()->loopbackPacketSender->sendToServer(&packet);
-				PlayerAuthInputPacket p;
-				p.pos.z = z;
-				//g_Data.getClientInstance()->loopbackPacketSender->sendToServer(&p);
-			}
-			clientMessageF("teleported");
-			teleported = true;
-			C_MovePlayerPacket packet(g_Data.getLocalPlayer(), *g_Data.getLocalPlayer()->getPos()); packet.Position = entPos;
-			g_Data.getClientInstance()->loopbackPacketSender->sendToServer(&packet);
-		}
-	}
-	else
-	{
-		//Continue tp
-		C_MovePlayerPacket packet(g_Data.getLocalPlayer(), *g_Data.getLocalPlayer()->getPos()); packet.Position = entPos;
-		//g_Data.getClientInstance()->loopbackPacketSender->sendToServer(&packet);
-		PlayerAuthInputPacket p;
-		p.pos = entPos;
-		//g_Data.getClientInstance()->loopbackPacketSender->sendToServer(&p);
-		clientMessageF("Continue teleported");
-	}
-
-
-	//Attack
-	if (g_Data.canUseMoveKeys() && !targetList.empty() /* && teleported*/) {
-		if (TimerUtil::hasTimedElapsed((1000 / APS), true)) {
-			if (!moduleMgr->getModule<NoSwing>()->isEnabled())
-				player->swing();
-				gm->attack(targetList[0]);
-				clientMessageF("attacked");
-
-			/*for (auto& i : targetList) {
-				// crashes
-				if (mode.getSelectedValue() == 0) gm->attack(i);
-				else gm->attack(targetList[0]);
-				clientMessageF("attacked");
-			}*/
-		}
-	}
-
-	//Setback
-	if (ticks >= delay) {
-		//X
-		counter = gx - x;
-		if (0 < gx - x) {
-			isplus = true;
-		}
-		else
+	sort(targetList.begin(), targetList.end(), CompareTargetEnArray());
+	if (g_Data.canUseMoveKeys() && !targetList.empty()) {
+		vec3_t pos = player->currentPos;
+		if (!back) savepos = player->currentPos;
+		vec3_t pos2 = targetList[0]->currentPos;
+		float yawRad = (targetList[0]->yaw + 90) * (PI / 180);
+		float pitchRad = (targetList[0]->pitch) * -(PI / 180);
+		if (behind)
+			pos2 = pos2.add(
+				(cos(yawRad) * cos(pitchRad)) * -1.5f,
+				0,
+				(sin(yawRad) * cos(pitchRad)) * -1.5f
+			);
+		pos2 = pos2.add(0, height, 0);
+		float dist = pos.dist(pos2);
+		float backdist = pos.dist(savepos);
+		if (!back)
 		{
-			isplus = false;
-			counter = 0 - counter;
-		}
-		for (int i = 0; i < counter; i++) {
-			if (isplus) {
-				x += 1;
+			check = 0;
+			for (auto& target : targetList)
+			{
+				check++;
+				pos = player->currentPos;
+				vec3_t pos2 = target->currentPos;
+				float yawRad = (target->yaw + 90) * (PI / 180);
+				float pitchRad = (target->pitch) * -(PI / 180);
+				if (behind)
+					pos2 = pos2.add(
+						(cos(yawRad) * cos(pitchRad)) * -1.5f,
+						0,
+						(sin(yawRad) * cos(pitchRad)) * -1.5f
+					);
+				pos2 = pos2.add(0, height, 0);
+				float dist = pos.dist(pos2);
+				for (int i = 0; i < dist * 1.f; i++)
+				{
+					vec3_t pos3 = pos.lerp(pos2, i / (float)(dist * 1.f));
+					PlayerAuthInputPacket p = PlayerAuthInputPacket(pos3, player->pitch, player->yaw, player->yawUnused1);
+					g_Data.getClientInstance()->loopbackPacketSender->sendToServer(&p);
+					if (!spoof) player->setPos(pos3);
+				}
+				if (!moduleMgr->getModule<NoSwing>()->isEnabled())
+					player->swing();
+				gm->attack(target);
+				back = true;
+				realdelay = bdelay;
+				if (check >= multi) break;
 			}
-			else {
-				x -= 1;
-			}
-			C_MovePlayerPacket packet(g_Data.getLocalPlayer(), *g_Data.getLocalPlayer()->getPos()); packet.Position.x = x;
-			g_Data.getClientInstance()->loopbackPacketSender->sendToServer(&packet);
-			PlayerAuthInputPacket p;
-			p.pos.x = x;
-			//g_Data.getClientInstance()->loopbackPacketSender->sendToServer(&p);
 		}
-
-		//Y
-		counter = gy - y;
-		if (0 < gy - y) {
-			isplus = true;
-		}
-		else
+		else if (backsettings)
 		{
-			isplus = false;
-			counter = 0 - counter;
-		}
-		for (int i = 0; i < counter; i++) {
-			if (isplus) {
-				y += 1;
+			for (int i = 0; i < backdist * 1.f; i++)
+			{
+				vec3_t pos3 = pos.lerp(savepos, i / (float)(backdist * 1.f));
+				PlayerAuthInputPacket p = PlayerAuthInputPacket(pos3, player->pitch, player->yaw, player->yawUnused1);
+				g_Data.getClientInstance()->loopbackPacketSender->sendToServer(&p);
+				if (!spoof) player->setPos(pos3);
 			}
-			else {
-				y -= 1;
-			}
-			C_MovePlayerPacket packet(g_Data.getLocalPlayer(), *g_Data.getLocalPlayer()->getPos()); packet.Position.y = pos.y;
-			g_Data.getClientInstance()->loopbackPacketSender->sendToServer(&packet);
-			PlayerAuthInputPacket p;
-			p.pos.y = y;
-			//g_Data.getClientInstance()->loopbackPacketSender->sendToServer(&p);
+			back = false;
+			realdelay = delay;
 		}
-
-		//Z
-		counter = gz - z;
-		if (0 < gz - z) {
-			isplus = true;
-		}
-		else
-		{
-			isplus = false;
-			counter = 0 - counter;
-		}
-		for (int i = 0; i < counter; i++) {
-			if (isplus) {
-				z += 1;
-			}
-			else {
-				z -= 1;
-			}
-			C_MovePlayerPacket packet(g_Data.getLocalPlayer(), *g_Data.getLocalPlayer()->getPos()); packet.Position.z = z;
-			g_Data.getClientInstance()->loopbackPacketSender->sendToServer(&packet);
-			PlayerAuthInputPacket p;
-			p.pos.z = z;
-			//g_Data.getClientInstance()->loopbackPacketSender->sendToServer(&p);
-		}
-
-
-		teleported = false;
-		ticks = 0;
-		clientMessageF("set back");
-		C_MovePlayerPacket packet(g_Data.getLocalPlayer(), *g_Data.getLocalPlayer()->getPos()); packet.Position = pos;
-		g_Data.getClientInstance()->loopbackPacketSender->sendToServer(&packet);
 	}
-}
-
-void TPAura::onPostRender(C_MinecraftUIRenderContext* renderCtx) {
-}
-
-void TPAura::onLevelRender() {
-}
-
-void TPAura::onSendPacket(C_Packet* packet) {
-	auto player = g_Data.getLocalPlayer();
-	if (player == nullptr) return;
-
-	if (packet->isInstanceOf<C_MovePlayerPacket>()) {
-		auto* movePacket = reinterpret_cast<C_MovePlayerPacket*>(packet);
-		auto* AuthinputPacket = reinterpret_cast<PlayerAuthInputPacket*>(packet);
-
-		static int ticksSinceTeleport = 0;
-		/*if (g_Data.canUseMoveKeys() && !targetList.empty()) {
-			vec3_t entPos = *targetList[0]->getPos();
-			vec3_t pos = *player->getPos();
-
-			if (ticks % delay == 0) {
-				clientMessageF("teleported");
-				movePacket->Position.x = entPos.x;
-				movePacket->Position.y = entPos.y;
-				movePacket->Position.z = entPos.z;
-				AuthinputPacket->pos.x = entPos.x;
-				AuthinputPacket->pos.y = entPos.y;
-				AuthinputPacket->pos.z = entPos.z;
-				teleported = true;
-				ticksSinceTeleport++;
-			}
-
-			if (ticksSinceTeleport >= 5) {
-				teleported = false;
-				clientMessageF("set back");
-				movePacket->Position.x = pos.x; movePacket->Position.y = pos.y; movePacket->Position.z = pos.z;
-				AuthinputPacket->pos.x = pos.x; AuthinputPacket->pos.y = pos.y; AuthinputPacket->pos.z = pos.z;
-				ticksSinceTeleport = 0;
-			}
-		}*/
-	}
+	if (once)
+		setEnabled(false);
 }
 
 void TPAura::onDisable() {
 	auto player = g_Data.getLocalPlayer();
 	if (player == nullptr) return;
-	C_MovePlayerPacket packet(g_Data.getLocalPlayer(), *g_Data.getLocalPlayer()->getPos()); packet.Position = *player->getPos();
-	g_Data.getClientInstance()->loopbackPacketSender->sendToServer(&packet);
-	PlayerAuthInputPacket p;
-	p.pos = *player->getPos();
-	//g_Data.getClientInstance()->loopbackPacketSender->sendToServer(&p);
 }
