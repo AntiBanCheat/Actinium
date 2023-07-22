82ec7bd3aa921e38ca8009644205f40c0fb9f186
AntiBanCheat <94786995+AntiBanCheat@users.noreply.github.com>
2023-07-22 15:44:56 +0900

    Update ChatSuffix.cpp

--- Client/PacketClient/Module/Modules/Other/ChatSuffix.cpp
+++ Client/PacketClient/Module/Modules/Other/ChatSuffix.cpp
@@ -19,13 +19,9 @@ void ChatSuffix::onSendPacket(C_Packet* packet) {
 		std::string Sentence;
 		std::string end;
 		int i = random(1, 40);
-		if (i == 1) end = " | Aceolus";
-		if (i == 2) end = " | Skidders V3";
-		if (i == 3) end = " | Radium V2";
-		if (i >= 4 && i <= 6) end = " | A C T I N I U M";
-		if (i > 6) end = " | Actinium";
+		if (i >= 0 && i <= 3) end = " | A C T I N I U M";
+		if (i > 3) end = " | Actinium";
 #ifdef _DEBUG
-		end += " Beta";
 #endif // _DEBUG
 		std::string start;
 		if (bypass && !moduleMgr->getModule<ChatBypass>()->isEnabled()) {
